0feeb3647c0f302808503b88df7649cb7bba4412
fsuleWitdata <fsule@witdata.com.ar>
2025-10-11 17:02:21 -0300

    ADD: image

--- l10n_ar_bcra_credit_check/__manifest__.py
+++ l10n_ar_bcra_credit_check/__manifest__.py
@@ -2,6 +2,7 @@
     'name': 'BCRA Credit Check',
     'version': '19.0.1.1.0',
     'category': 'Accounting',
+    'images':['static/description/main_screenshot.png'],
     'summary': 'Consulta el estado crediticio, historial y cheques rechazados del cliente en el BCRA',
     'author': 'Horacio Montaño, Francisco Sulé',
     'depends': ['account','contacts'],
